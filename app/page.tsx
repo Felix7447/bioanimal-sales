44514b99151d90dbd3a5b8c1c9d4c6825aae411c
Felix Reyna <felixreyna48@gmail.com>
2023-11-17 17:48:04 -0400

    feat: :sparkles: Added Table Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 import { CardComponent } from '@/components/Card'
 import { ChartComponent } from '@/components/Chart'
+import { TableComponent } from '@/components/Table'
 
 export default function Home() {
   return (
     <>
-      <main className="p-4 col-span-12 flex flex-col items-center md:col-span-8 lg:col-span-7 border-2 border-black">
+      <main className="p-4 col-span-12 flex flex-col items-center md:col-span-8 lg:col-span-7">
         <h1 className="text-xl font-bold my-4 self-start">Dashboard</h1>
         <header className="relative flex justify-around w-full mx-2">
           <CardComponent background="#ddf2fe" title="Total Sales" sales={500} />
           <CardComponent background="#eefea2" title="Vet Sales" sales={250} />
           <CardComponent background="#e9ddfc" title="Peluq Sales" sales={250} />
         </header>
-        <main className="w-full">
+        <main className="w-full bg-base my-10 rounded-lg">
           <ChartComponent />
         </main>
-        <footer>Top clients</footer>
+        <footer className="w-full">
+          <h3 className="text-xl font-bold text-left">Top clients</h3>
+          <section className="bg-base my-4 rounded-lg">
+            <TableComponent />
+          </section>
+        </footer>
       </main>
       <aside className="min-h-screen p-4 col-span-12 flex flex-col justify-evenly md:col-span-4 lg:col-span-3 border-2 border-black">
         <main>Stats</main>
---
+++ components/Table/index.tsx
@@ -0,0 +1,62 @@
+"use client"
+import React from "react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from "@nextui-org/react";
+
+const rows = [
+  {
+    key: "1",
+    name: "Tony Reichert",
+    pet: "Camila",
+    type: "Veterinaria",
+  },
+  {
+    key: "2",
+    name: "Zoey Lang",
+    pet: "Uñañe",
+    type: "Peluquería",
+  },
+  {
+    key: "3",
+    name: "Jane Fisher",
+    pet: "Sindy",
+    type: "Veterinaria",
+  },
+  {
+    key: "4",
+    name: "William Howard",
+    pet: "Commy",
+    type: "Peluquería",
+  },
+];
+
+const columns = [
+  {
+    key: "name",
+    label: "NOMBRE",
+  },
+  {
+    key: "pet",
+    label: "MASCOTA",
+  },
+  {
+    key: "type",
+    label: "TIPO",
+  },
+];
+
+export const TableComponent = () => {
+  return (
+    <Table aria-label="Ultimas ventas">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key} className="cursor-pointer hover:text-white hover:bg-neutral duration-250">
+            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
